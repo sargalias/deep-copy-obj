8e04a102231fab46c02acda5d1ca618289086887
Spyros Argalias <sargalias@gmail.com>
2019-08-25 00:51:52 +0100

    config(TypeScript): Add config for TypeScript

--- webpack.common.js
+++ webpack.common.js
@@ -2,9 +2,21 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        use: ['ts-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
