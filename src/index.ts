9e13f1e6a2a1ed0c26e69c37e9953a50acc00737
Spyros Argalias <sargalias@gmail.com>
2019-08-25 20:05:51 +0100

    feat(deepCopyObj): Add case - equality - flat object with null and undefined

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ interface Obj {
 }
 
 const deepCopyObj = (obj?: Obj) => {
-  if (!obj) {
+  if (obj === undefined) {
     return undefined;
   }
 
   const result: Obj = {};
   Object.entries(obj).forEach(([key, value]) => {
-    if (typeof value === 'object') {
+    if (typeof value === 'object' && value !== null) {
       result[key] = deepCopyObj(value);
     } else {
       result[key] = value;
